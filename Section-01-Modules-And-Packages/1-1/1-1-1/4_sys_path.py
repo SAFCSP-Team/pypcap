a1a5c59bdeb409eb82bbf5ab7f639f712754406e
fahadLine <152792332+fahadLine@users.noreply.github.com>
2024-03-05 16:27:17 +0300

    Q2

---
+++ Section-01-Modules-And-Packages/1-1/1-1-1/4_sys_path.py
@@ -0,0 +1,80 @@
+
+# Q1: How can I view the contents of sys.path?
+
+
+# Q2: How can I add a directory to the sys.path?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# A1: You can view the contents of sys.path by importing the sys module and printing the sys.path list. Here's an example:
+# import sys
+# print(sys.path)
+
+
+
+# A2 Add a custom directory to sys.path
+# import sys
+# sys.path.append('/path/to/custom/directory')
+
